25d5838c48e6ee142a61450f549b5c8d2928ce99
Samuel Vilkovsky <samuel.vilkovsky.2@student.tuke.sk>
2024-04-07 13:45:20 +0200

    done graphics

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+import os
+
+# 1. PART - Set camera settings and start capturing video
+cap = cv2.VideoCapture(0);
+cap.set(3, 640);
+cap.set(4, 480);
+
+# 2. PART - Read the background image
+imgBackground = cv2.imread("Resources/background.png")
+
+# 3. PART - Read the mode images into a list
+folderModePath = 'Resources/Modes'
+modePath = os.listdir(folderModePath)
+imgModeList = []
+
+for path in modePath:
+    imgModeList.append(cv2.imread(f'{folderModePath}/{path}'))
+# print(len(imgModeList))
+
+while True:
+    success, img = cap.read()
+
+    imgBackground[162:162+480, 55:55+640] = img # Overlay the background image with the camera image
+    imgBackground[44:44+633,808:808+414] = imgModeList[0] # Overlay the background image with the mode image
+
+    cv2.imshow("Face Attendance", imgBackground) # Show the background image
+    cv2.waitKey(1)
